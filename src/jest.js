4b48bee10378266d86e97d4377340e032ace76fc
Gmounika77 <110013836+Gmounika77@users.noreply.github.com>
2025-04-21 17:07:21 +0530

    Create jest.js

---
+++ src/jest.js
@@ -0,0 +1,24 @@
+function isValidEmployee(employee) {
+  return (
+    typeof employee.name === 'string' &&
+    typeof employee.salary === 'number' &&
+    employee.salary > 0
+  );
+}
+
+function calculateBonus(employee, percentage) {
+  if (!isValidEmployee(employee)) {
+    throw new Error('Invalid employee');
+  }
+  return (employee.salary * percentage) / 100;
+}
+
+function formatEmployeeInfo(employee) {
+  return `${employee.name} earns $${employee.salary.toFixed(2)} per year.`;
+}
+
+module.exports = {
+  isValidEmployee,
+  calculateBonus,
+  formatEmployeeInfo
+};
